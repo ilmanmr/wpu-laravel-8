9fe4ee51f4956d06463a076974c26d8c1c06ff57
Ilman Manba'ullum Ramdhan <44298891+ilmanmr@users.noreply.github.com>
2021-08-16 23:29:25 +0700

    Belajar Laravel 8 - 15. User Registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,5 +14,23 @@ class RegisterController extends Controller
             'title' => 'Register',
             'active' => 'register'
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users', 
+            'password' => 'required|min:5|max:255', 
+        ]);
+
+
+        $validateData['password'] = Hash::make($validateData['password']);
+
+        User::create($validateData);
+
+        //$request->session()->flash('success', 'Registration successfull! Please Login!');
+        return redirect('/login')->with('success', 'Registration successfull! Please Login!');
     }
 }
