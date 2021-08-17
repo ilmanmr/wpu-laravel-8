dd743ce67a66c4faa0d468cd24b9a3ae3992e8d0
Ilman Manba'ullum Ramdhan <44298891+ilmanmr@users.noreply.github.com>
2021-08-17 12:04:39 +0700

    Belajar Laravel 8 - 16. User Login & Middleware

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,1 @@
+<h1>dsadadasda</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
@@ -36,6 +37,11 @@ Route::get('/categories', function () {
     ]);
 });
 
-Route::get('/login', [LoginController::class, 'index']);
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->middleware('guest')->name('login');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
