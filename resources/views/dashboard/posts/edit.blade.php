e65f28a278c6f8a00518e5d358aef4951a80482d
Ilman Manba'ullum Ramdhan <44298891+ilmanmr@users.noreply.github.com>
2021-10-04 09:46:25 +0700

    Belajar Laravel 8 - 22. Preview, Update & Delete Image

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -6,7 +6,7 @@
 </div>
 
 <div class="col-lg-8">
-    <form method="POST" action="/dashboard/posts/{{ $post->slug }}" class="mb-5">
+    <form method="POST" action="/dashboard/posts/{{ $post->slug }}" class="mb-5" enctype="multipart/form-data">
         @method('put')
         @csrf
         <div class="mb-3">
@@ -36,6 +36,17 @@
             </select>
         </div>
         <div class="mb-3">
+            <label for="image" class="form-label">Post Image</label>
+            <input type="hidden" name="oldImage" value="{{ $post->image }}">
+            <img class="img-preview img-fluid mb-3 col-sm-5 d-block" src="{{ $post->image ? asset('storage/' . $post->image) : '' }}">
+            <input class=" form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
+            @error('image')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="mb-3">
             <label for="body" class="form-label">Body</label>
             @error('body')
             <p class="text-danger">{{ $message }}</p>
@@ -60,5 +71,19 @@
     document.addEventListener('trix-file-accept', function(e) {
         e.preventDefault()
     })
+
+    function previewImage() {
+        const image = document.querySelector('#image');
+        const imgPreview = document.querySelector('.img-preview');
+
+        imgPreview.style.display = 'block';
+
+        const oFReader = new FileReader();
+        oFReader.readAsDataURL(image.files[0]);
+
+        oFReader.onload = function(oFREvent) {
+            imgPreview.src = oFREvent.target.result;
+        }
+    }
 </script>
 @endsection
